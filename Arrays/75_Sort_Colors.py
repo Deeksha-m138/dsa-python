5b65fc176da5345cbd6a75324f9b516c3948862a
Deeksha-m138 <b23cs138@kitsw.ac.in>
2026-06-29 12:01:26 +0530

    Majority element

--- Arrays/75_Sort_Colors.py
+++ Arrays/75_Sort_Colors.py
@@ -1,11 +1,11 @@
 i,j,k=0,0,len(nums)-1
-        while i<=k:
-            if nums[i]==0:
-                nums[i],nums[j]=nums[j],nums[i]
-                j+=1
-                i+=1
-            elif nums[i]==2:
-                nums[i],nums[k]=nums[k],nums[i]
-                k-=1
-            else: 
-                i+=1
+while i<=k:
+    if nums[i]==0:
+        nums[i],nums[j]=nums[j],nums[i]
+        j+=1
+        i+=1
+    elif nums[i]==2:
+        nums[i],nums[k]=nums[k],nums[i]
+        k-=1
+    else: 
+        i+=1

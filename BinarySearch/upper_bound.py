57b0c1d6d41bbd972b81acdbe93f5cb2b2be9151
Deeksha-m138 <b23cs138@kitsw.ac.in>
2026-07-10 00:20:22 +0530

    Solve: Find upper bound in sorted array

--- BinarySearch/upper_bound.py
+++ BinarySearch/upper_bound.py
@@ -1,11 +1,12 @@
 nums=list(map(int,input().split()))
+target=int(input())
 low,high=0,len(nums)-1
 while low<=high:
     mid=(high+low)//2
-    if nums[mid]>=target:
+    if nums[mid]>target:
         high=mid-1
     else:
         low=mid+1
 print(low)
 
-""" TC=O(logn) SC=O(1) """
+""" TC=O(logn) SC=O(1)  """
